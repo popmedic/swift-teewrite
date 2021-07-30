ca7d295176f8eeeb31a44da518bbaaa00613cb57
Kevin Scardina <kevin.scardina@charter.com>
2021-07-30 14:44:35 -0600

    clean up comments

--- Tests/TeeWriteTests/TeeWriterTests.swift
+++ Tests/TeeWriteTests/TeeWriterTests.swift
@@ -5,30 +5,42 @@ class TeeWriterTests: XCTestCase {
 
     // Test the TeeWriter when just using a update handler for when a write to handle
     func testTeeWriter() throws {
-        // assign
-        var act = "" // will hold what actually was written
-        let exp = "this is a test" // we expect to get this in act
-        let expectation = XCTestExpectation() // pause to make sure the writing is done
+        // MARK: assign
+        // will hold what actually was written
+        var act = ""
+        // we expect to get this in act
+        let exp = "this is a test"
+        // pause to make sure the writing is done
+        let expectation = XCTestExpectation()
         // we will write to /dev/null (nothing)
         let file = FileHandle(fileDescriptor: fileno(fopen("/dev/null", "a")))
         // setup our TeeWriter to...
         _ = TeeWriter(handle: file) { data in
             // append anything writen to /dev/null to act
             act += String(data: data, encoding: .ascii) ?? "error"
-            expectation.fulfill() // finish up waiting
+            // finish up waiting
+            expectation.fulfill()
         }
-        file.write(exp.data(using: .ascii)!) // write the expected to the file
-        wait(for: [expectation], timeout: 0.2) // wait for the tee write to finish
-        // assert
-        XCTAssertEqual(act, exp) // make sure we actual got what was expected!
+        
+        // MARK: act
+        // write the expected to the file
+        file.write(exp.data(using: .ascii)!)
+        // wait for the tee write to finish
+        wait(for: [expectation], timeout: 0.2)
+        
+        // MARK: assert
+        // make sure we actual got what was expected!
+        XCTAssertEqual(act, exp)
     }
 
     // Test the TeeWriter init with handle and also handler
     // (when writing to handle also writes to other)
     func testTeeWriterAlsoOther() throws {
-        // assign
-        let expectation = XCTestExpectation() // pause to make sure the writing is done
-        let exp = "this is a test.".data(using: .ascii)! // expected value
+        // MARK: assign
+        // pause to make sure the writing is done
+        let expectation = XCTestExpectation()
+        // expected value
+        let exp = "this is a test.".data(using: .ascii)!
         // temp file urls
         let temp1 = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("temp1.txt")
         let temp2 = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent("temp2.txt")
@@ -36,19 +48,28 @@ class TeeWriterTests: XCTestCase {
         let file1 = fopen(temp1.path, "a+")
         let file2 = fopen(temp2.path, "a+")
         // set up the file handles
-        let handle1 = FileHandle(fileDescriptor: fileno(file1)) // this one will be written to
-        let handle2 = FileHandle(fileDescriptor: fileno(file2)) // this one should be written to
+        // this one will be written to
+        let handle1 = FileHandle(fileDescriptor: fileno(file1))
+        // this one should be written to
+        let handle2 = FileHandle(fileDescriptor: fileno(file2))
         // create the TeeWriter to test, anything written to handle1 also writes to handle2
         _ = TeeWriter(handle1, also: handle2, written: { _ in expectation.fulfill() })
-        // act
-        handle1.write(exp)  // write to handle1, should also write to handle2 if TeeWriter works
+        
+        // MARK: act
+        // write to handle1, should also write to handle2 if TeeWriter works
+        handle1.write(exp)
         // wait for the write to be preformed
         wait(for: [expectation], timeout: 0.2)
-        // assert
-        try handle2.seek(toOffset: 0) // seek to the start of handle2
-        let act = try handle2.readToEnd()  // read everything in handle2's file
-        XCTAssertEqual(act, exp) // is our actual value the same as the expected value?
-        // annihilate
+        
+        // MARK: assert
+        // seek to the start of handle2
+        try handle2.seek(toOffset: 0)
+        // read everything in handle2's file
+        let act = try handle2.readToEnd()
+        // is our actual value the same as the expected value?
+        XCTAssertEqual(act, exp)
+        
+        // MARK: annihilate
         try FileManager.default.removeItem(at: temp1) // remove temp1.txt
         try FileManager.default.removeItem(at: temp2) // remove temp2.txt
     }
