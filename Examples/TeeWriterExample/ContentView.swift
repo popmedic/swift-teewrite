4b99cbcbecf4b929b347a7fa17e4f52b9e208079
Kevin Scardina <kevin.scardina@charter.com>
2021-07-30 17:43:23 -0600

    teewrite stderr as well.

--- Examples/TeeWriterExample/ContentView.swift
+++ Examples/TeeWriterExample/ContentView.swift
@@ -7,9 +7,17 @@ class StdoutTeeWrite: ObservableObject {
     // this will buffer overflow.  Maybe only hold 1024 characters or something.
     // Even NSCache comes to mind as a way to hold without using too much RAM.
     @Published var text = ""
-    private var teeWriter: TeeWriter!
+    private var outTeeWriter: TeeWriter!
+    private var errTeeWriter: TeeWriter!
     init() {
-        teeWriter = TeeWriter(handle: FileHandle.standardOutput) { [weak self] data in
+        setvbuf(stdout, nil, _IONBF, 0)
+        outTeeWriter = TeeWriter(handle: FileHandle.standardOutput) { [weak self] data in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.text += String(data: data, encoding: .ascii) ?? "bad data"
+            }
+        }
+        errTeeWriter = TeeWriter(handle: FileHandle.standardError) { [weak self] data in
             guard let self = self else { return }
             DispatchQueue.main.async {
                 self.text += String(data: data, encoding: .ascii) ?? "bad data"
@@ -25,7 +33,7 @@ struct ContentView: View {
         VStack {
             TextField("text to print", text: $input).padding()
             Button("print") {
-                print(input)
+                NSLog(input)
             }.padding()
             ScrollView {
                 Text(stdoutTeeRead.text).padding().frame(maxWidth: .infinity)
